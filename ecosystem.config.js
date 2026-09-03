a04aa0e924d30b1aac2b47ccaef9f90bcd06e87d
Sachinnn22 <sachinduchirau2@gmail.com>
2026-09-03 20:14:37 +0530

    changed platform

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,20 +3,20 @@ module.exports = {
         {
             name: "config-server",
             script: "java",
-            args: "-jar ./config-server/target/config-server-0.0.1-SNAPSHOT.jar",
+            args: "-Xms128m -Xmx256m -jar ./config-server/target/config-server-0.0.1-SNAPSHOT.jar",
             log_file: "./logs/config-server.log",
         },
         {
             name: "service-registry",
             script: "java",
-            args: "-jar ./service-registry/target/service-registry-0.0.1-SNAPSHOT.jar",
+            args: "-Xms128m -Xmx256m -jar ./service-registry/target/service-registry-0.0.1-SNAPSHOT.jar",
             log_file: "./logs/service-registry.log",
             restart_delay: 10000, 
         },
         {
             name: "api-gateway",
             script: "java",
-            args: "-jar ./api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar",
+            args: "-Xms128m -Xmx256m -jar ./api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar",
             log_file: "./logs/api-gateway.log",
             restart_delay: 20000,
         }
